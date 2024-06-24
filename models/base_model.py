447e7c889df5a716873e32bfd938f053672cbb83
Isaiah woko <wokoisaiah68@gmail.com>
2024-06-24 12:29:45 +0100

    made changes to sqlalchemy

--- models/base_model.py
+++ models/base_model.py
@@ -3,18 +3,29 @@
 import uuid
 from datetime import datetime
 import models
+from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
+
 Base = declarative_base()
+
+
 class BaseModel:
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
         """
         *args, **kwargs arguments for the constructor of a BaseModel
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()  # I think this doesn't\
+        # i updated the class attrbutes id, created_at and updated_at\
+        # based on requirements. I have also backed up your original code
+        # elsewhere incase you need to make changes\
+
+        self.id = Column(String(60), primary_key=True, nullable=False,
+                         default=str(uuid.uuid4()))
+        self.created_at = Column(DateTime, nullable=False,
+                                 default=datetime.now)
+        self.updated_at = Column(DateTime, nullable=False,
+                                 default=datetime.now)  # I think this doesn't\
         # need to exist in the requirement: otherwise:\
         # create id and created_at as you did previously (new instance)
         if kwargs:
@@ -30,8 +41,6 @@ class BaseModel:
             if "__class__" in kwargs:
                 del kwargs['__class__']
             self.__dict__.update(kwargs)
-        else:
-            models.storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -40,9 +49,9 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
@@ -52,4 +61,9 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        if '_sa_instance_state' in dictionary:
+            del dictionary['_sa_instance_state']
         return dictionary
+
+    def delete(self):
+        models.storage.delete(self)
--- models/city.py
+++ models/city.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, DateTime
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = ""
+    __tablename__ = 'cities'
+    state_id = column()
     name = ""
