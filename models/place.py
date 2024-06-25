58446cb883aee27000e5d6efe2b5d1c677cdc32a
Isaiah woko <wokoisaiah68@gmail.com>
2024-06-25 16:38:41 +0100

    updated task 9

--- models/place.py
+++ models/place.py
@@ -33,17 +33,18 @@ class Place(BaseModel, Base):
                 if review.place_id == self.id:
                     list_of_review.append(review)
             return list_of_review
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
 
     def __init__(self, *args, **kwargs):
         """Initializes user"""
