e531a8d2c248f98221c30cd244c2ea0943561e54
Isaiah woko <wokoisaiah68@gmail.com>
2024-06-25 15:36:45 +0100

    task 8

--- models/place.py
+++ models/place.py
@@ -13,10 +13,10 @@ class Place(BaseModel, Base):
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
         description = Column(String(1024), nullable=False)
-        number_rooms = Column(Integer, nullable=False, default='0')
-        number_bathrooms = Column(Integer, nullable=False, default='0')
-        max_guest = Column(Integer, nullable=False, default='0')
-        price_by_night = Column(Integer, nullable=False, default='0')
+        number_rooms = Column(Integer, nullable=False, default=0)
+        number_bathrooms = Column(Integer, nullable=False, default=0)
+        max_guest = Column(Integer, nullable=False, default=0)
+        price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float, nullable=False)
         longitude = Column(Float, nullable=False)
         amenity_ids = []
@@ -33,6 +33,6 @@ class Place(BaseModel, Base):
         longitude = 0.0
         amenity_ids = []
 
-    def __init__(self, *args, **kwargs):
-        """Initializes user"""
-        super().__init__(*args, **kwargs)
+    # def __init__(self, *args, **kwargs):
+    #     """Initializes user"""
+    #     super().__init__(*args, **kwargs)
