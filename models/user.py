2cca9d9f02750a323c69dafb121be1af2b5617bb
Isaiah woko <wokoisaiah68@gmail.com>
2024-06-25 15:40:32 +0100

    updated task 8

--- models/user.py
+++ models/user.py
@@ -14,8 +14,7 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship('places', backref='user', cascade='all,\
-                              delete, delete-orphan')
+        places = relationship('Place', backref='user', cascade='all, delete, delete-orphan')
     else:
         email = ""
         password = ""
