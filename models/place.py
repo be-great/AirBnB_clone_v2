0077cf6e0ca3a3c2a4e3853e5728bfc46917982b
be-great <begreat5333@gmail.com>
2024-06-27 00:11:15 +0300

    reset head

--- models/place.py
+++ models/place.py
@@ -4,48 +4,30 @@ from models.base_model import BaseModel, Base
 from models import storage_ob
 from sqlalchemy.orm import relationship
 # from models.review import Review
-# import models
+import models
 
 
 class Place(BaseModel, Base):
-    """ A place to stay """
-    if storage_ob == "db":
+    """ Representation of a Place """
+
+    if models.storage_t == 'db':
         __tablename__ = 'places'
         city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
-        description = Column(String(1024), nullable=True)
-        number_rooms = Column(Integer, nullable=False, default=0)
-        number_bathrooms = Column(Integer, nullable=False, default=0)
-        max_guest = Column(Integer, nullable=False, default=0)
-        price_by_night = Column(Integer, nullable=False, default=0)
-        latitude = Column(Float, nullable=True)
-        longitude = Column(Float, nullable=True)
-        amenity_ids = []
+        description = Column(String(1024))
+        number_rooms = Column(Integer, default=0, nullable=False)
+        number_bathrooms = Column(Integer, default=0, nullable=False)
+        max_guest = Column(Integer, default=0, nullable=False)
+        price_by_night = Column(Integer, default=0, nullable=False)
+        latitude = Column(Float)
+        longitude = Column(Float)
 
-        place_amenity = Table('place_amenity', Base.metadata,
-                              Column(String(60), 'place_id', ForeignKey('places.id'), primary_key=True, nullable=False),
-                              Column(String(60), 'amenity_id', ForeignKey('amenities.id'), primary_key=True, nullable=False)
-                              )
+        reviews = relationship("Review", backref="place")
+        amenities = relationship("Amenity", secondary="place_amenity",
+                                 backref="place_amenities")
 
-        amenities = relationship(
-            'Amenity',
-            seconadry=place_amenity,
-            back_populates='place_amenities',
-            viewonly=False
-        )
-        # reviews = relationship('Review', backref='place', cascade='all, delete, delete-orphan')
     else:
-        # @property
-        # def reviews(self):
-        #     """This is a Getter attribute that returns the list of Review
-        #           instances with place_id equals to the current Place.id"""
-        #     list_of_review = []
-        #     for review in storage.all(Review).values():
-        #         if review.place_id == self.id:
-        #             list_of_review.append(review)
-        #     return list_of_review
-
         city_id = ""
         user_id = ""
         name = ""
@@ -57,6 +39,21 @@ class Place(BaseModel, Base):
         latitude = 0.0
         longitude = 0.0
         amenity_ids = []
+
+    if models.storage_t != 'db':
+        @property
+        def reviews(self):
+            """ Getter attribute that returns the list of Review instances """
+            from models.review import Review
+            return [review for review in models.storage.all(Review).values()
+                    if review.place_id == self.id]
+
+        @property
+        def amenities(self):
+            """ Getter attribute that returns the list of Amenity instances """
+            from models.amenity import Amenity
+            return [amenity for amenity in models.storage.all(Amenity).values()
+                    if amenity.place_id == self.id]
 
     def __init__(self, *args, **kwargs):
         """Initializes user"""
