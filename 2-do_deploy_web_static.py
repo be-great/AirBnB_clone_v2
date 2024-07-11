dc8462f2caf0cf94e4c9bfbe08597ad0a474d275
be-great <begreat5333@gmail.com>
2024-07-12 01:06:11 +0300

    task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ 1. Compress before sending"""
-from fabric import task, Connection
+from fabric.api import local, env, put, run
 import os
 
 
@@ -14,19 +14,16 @@ def do_deploy(archive_path):
     try:
         arch_name = os.path.basename(archive_path)
         arch_base = os.path.splitext(arch_name)[0]
-
-        for h in env.hosts:
-            c = Connection(h)
-            # upload it to the archive 
-            c.put(archive_path, "/tmp/")
-            # uncompress archive
-            c.run(f"tar -xzf /tmp/{arch_name} -C /data/web_static/releases/{arch_base}")
-            # Delete the archive
-            c.run(f"rm /tmp/{arch_name}")
-            # Delete the symbolic link
-            c.run(f"rm /data/web_static/current")
-            # create the new symbolic link
-            c.run(f"ln -s /data/web_static/releases/{arch_base} /data/web_static/current")
+        # upload it to the archive 
+        put(archive_path, "/tmp/")
+        # uncompress archive
+        run(f"tar -xzf /tmp/{arch_name} -C /data/web_static/releases/{arch_base}")
+        # Delete the archive
+        run(f"rm /tmp/{arch_name}")
+        # Delete the symbolic link
+        run(f"rm /data/web_static/current")
+        # create the new symbolic link
+        run(f"ln -s /data/web_static/releases/{arch_base} /data/web_static/current")
         return True
     except Exception as e:
         return False
