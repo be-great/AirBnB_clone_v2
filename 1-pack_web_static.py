adcbd98f3025319d367003e6d0c135cc58ec3e58
be-great <begreat5333@gmail.com>
2024-07-11 21:47:39 +0300

    task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" 1. Compress before sending"""
+from fabric import task 
+from datetime import datetime
+import os
+
+
+@task
+def do_pack(c):
+    """create compression file"""
+    if not os.path.exists("versions"):
+        os.makedirs("versions")
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_name = f"versions/web_static_{timestamp}.tgz"
+
+    # Create the archive
+    try:
+        c.local(f"tar -czvf {archive_name} web_static")
+        return archive_name
+    except Exception as e:
+        return None
