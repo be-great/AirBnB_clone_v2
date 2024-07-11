21457144de8d2f748f5ae5c258183ac04aceceae
be-great <begreat5333@gmail.com>
2024-07-12 01:18:21 +0300

    task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -14,16 +14,20 @@ def do_deploy(archive_path):
     try:
         arch_name = os.path.basename(archive_path)
         arch_base = os.path.splitext(arch_name)[0]
-        # upload it to the archive 
+        # upload it to the archive
         put(archive_path, "/tmp/")
         # uncompress archive
-        run(f"tar -xzf /tmp/{arch_name} -C /data/web_static/releases/{arch_base}")
+        cmd0 = f"tar -xzf /tmp/{arch_name} -C "
+        cmd = cmd0 + "/data/web_static/releases/{arch_base}"
+        run(cmd)
         # Delete the archive
         run(f"rm /tmp/{arch_name}")
         # Delete the symbolic link
         run(f"rm /data/web_static/current")
         # create the new symbolic link
-        run(f"ln -s /data/web_static/releases/{arch_base} /data/web_static/current")
+        cmd0 = f"ln -s /data/web_static/releases/{arch_base} "
+        cmd = cmd0 + "/data/web_static/current"
+        run(cmd)
         return True
     except Exception as e:
         return False
