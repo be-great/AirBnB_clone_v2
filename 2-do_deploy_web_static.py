e07deabaf2c076ccded8903f533e3794e7e4f506
be-great <begreat5333@gmail.com>
2024-07-14 20:31:16 +0300

    task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,35 +9,22 @@ env.hosts = ['34.232.76.39', '52.91.132.184']
 
 def do_deploy(archive_path):
     """ Deploy a compressed archive to the web servers. """
-    if not os.path.exists(archive_path):
+    if not os.exists(archive_path):
         return False
+    arch_name = archive_path.split('/')[-1]
+    arch_base0 = '/data/web_static/releases/'
+    arch_base = arch_base0 + "{}".format(arch_name.split('.')[0])
+    tmp = "/tmp/" + arch_name
 
     try:
-        arch_name = os.path.basename(archive_path)
-        arch_base = os.path.splitext(arch_name)[0]
-
-        # Upload archive to /tmp/ directory on remote server
         put(archive_path, "/tmp/")
-
-        # Create directory for new release
-        run(f"mkdir -p /data/web_static/releases/{arch_base}")
-        cmd0 = f"tar -xzf /tmp/{arch_name} -C "
-        cmd = cmd0 + f"/data/web_static/releases/{arch_base}/"
-        # Extract archive to the new release directory
-        run(cmd)
-        # Remove the uploaded archive from /tmp/
-        run(f"rm /tmp/{arch_name}")
-        # mv the web_static
-        cmd0 = f"mv /data/web_static/releases/{arch_base}/web_static/* "
-        cmd = cmd0 + f"/data/web_static/releases/{arch_base}/"
-        run(cmd)
-        # Delete existing symbolic link
-        run(f"rm -rf /data/web_static/releases/{arch_base}/web_static")
-        run(f"rm -rf /data/web_static/current")
-        cmd0 = f"ln -s /data/web_static/releases/{arch_base}/ "
-        cmd = cmd0 + "/data/web_static/current"
-        # Create new symbolic link pointing to the new release
-        run(cmd)
+        run("mkdir -p {}/".format(arch_base))
+        run("tar -xzf {} -C {}/".format(tmp, arch_base))
+        run("rm {}".format(tmp))
+        run("mv {}/web_static/* {}/".format(arch_base, arch_base))
+        run("rm -rf {}/web_static".format(arch_base))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}/ /data/web_static/current".format(arch_base))
         return True
     except Exception as e:
         return False
