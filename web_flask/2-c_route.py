960840afefe5bbe97b53df11440c94ba8819a369
be-great <begreat5333@gmail.com>
2024-07-27 08:58:12 +0300

    task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 """a script that starts a Flask web application"""
-from flask import Flask
 
+
+from flask import Flask
 app = Flask(__name__)
-"""match both /name and /name/ and not only /name"""
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """display hello HBNB"""
+    """
+    match both /name and /name/ and not only /name
+    display hello HBNB
+    """
     return 'Hello HBNB!'
 
 
