2ba9206ec533071d28814d6a66e592681911bd1a
be-great <begreat5333@gmail.com>
2024-07-27 13:20:18 +0300

    task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -15,5 +15,11 @@ def list_states():
     return render_template("7-states_list.html", state=sorted_state)
 
 
+@app.termdown_appcontext
+def teardown_db(exception):
+    """closes the storage"""
+    storage.close()
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
