1b8c3c607aebc28271b7feaec896656880fa1629
be-great <begreat5333@gmail.com>
2024-07-29 03:30:25 +0300

    fdsfds

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,20 +4,20 @@ from flask import Flask, render_template
 from models import storage
 from models.state import State
 app = Flask(__name__)
-"""match both /name and /name/ and not only /name"""
-
-
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """display a HTML page with the states listed in alphabetical order"""
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
-    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
 def teardown_db(exception):
     """closes the storage"""
     storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def list_states():
+    """display HBNB"""
+    state = storage.all(State)
+    sorted_state = sorted(list(state.values()), key=lambda state: state.name)
+    return render_template("7-states_list.html", states=sorted_state)
 
 
 if __name__ == '__main__':
