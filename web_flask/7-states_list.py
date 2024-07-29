a35628f265c48d71a5dfdfe120ca95b75bab0a56
be-great <begreat5333@gmail.com>
2024-07-29 03:32:45 +0300

    scd

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,19 +4,21 @@ from flask import Flask, render_template
 from models import storage
 from models.state import State
 app = Flask(__name__)
-
-
-@app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage"""
-    storage.close()
+"""match both /name and /name/ and not only /name"""
 
 
 @app.route('/states_list', strict_slashes=False)
 def list_states():
     """display HBNB"""
     state = storage.all(State)
-    return render_template("7-states_list.html", states=state)
+    sorted_state = sorted(list(state.values()), key=lambda state: state.name)
+    return render_template("7-states_list.html", states=sorted_state)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage"""
+    storage.close()
 
 
 if __name__ == '__main__':
