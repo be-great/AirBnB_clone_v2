1dc2ba82eccd263ad16b6629e59005b11bb6479c
be-great <begreat5333@gmail.com>
2024-07-29 03:23:05 +0300

    df

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,6 +2,7 @@
 """a script that starts a Flask web application"""
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 app = Flask(__name__)
 """match both /name and /name/ and not only /name"""
 
@@ -9,8 +10,8 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def list_states():
     """display HBNB"""
-    sorted_state = sorted(list(storage.all("State").values()),
-                          key=lambda x: x.name)
+    state = storage.all(State)
+    sorted_state = sorted(list(state.values()), key=lambda state: state.name)
     return render_template("7-states_list.html", states=sorted_state)
 
 
